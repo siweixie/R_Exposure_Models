b3c429fecfa631322e398bedbeccc1bb6ff7548a
siweixie <146999962+siweixie@users.noreply.github.com>
2023-11-18 18:00:28 -0500

    Update Models_two_box_1.R

--- Models_two_box_1.R
+++ Models_two_box_1.R
@@ -1,1 +1,20 @@
+# Define model 200
+C_F <- gamma * G / Q # Far field
+C_N <- C_F + ((gamma * G) / beta) # Near field
 
+# Define model 201
+
+
+# Define the model parameters with units
+td <- 60    # minutes
+tg <- 15    # minutes
+G <- 100    # mg/min
+Q <- 20     # m^3/min
+QL <- 5     # m^3/min
+epsilon_L <- 0.5  # Local control efficiency
+epsilon_LF <- 0.75 # Filtration efficiency
+Q_R <- 5    # m^3/min
+V <- 100    # m^3
+V_N <- 8    # Near-field volume in m^3
+beta <- 5   # Near-field ventilation rate in m^3/min
+gamma <- 0.25
