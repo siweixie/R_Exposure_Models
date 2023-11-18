3a973bbeb4adb5fa4b1732c1c034d1f6b86091cd
siweixie <146999962+siweixie@users.noreply.github.com>
2023-11-18 18:06:02 -0500

    Update Models_two_box_1.R

--- Models_two_box_1.R
+++ Models_two_box_1.R
@@ -3,6 +3,31 @@ C_F <- gamma * G / Q # Far field
 C_N <- C_F + ((gamma * G) / beta) # Near field
 
 # Define model 201
+# Define Model 105
+model_201 <- function(G, Q, Q_L, epsilon_L, V, t_g, T) {
+  # Create a time vector
+  time_vector <- seq(0, T, by = 1)
+  
+  C_rise <- sapply(time_vector, function(t) {
+    if (t <= t_g) {
+      return(((G * (1 - epsilon_L)) / (Q + Q_L)) * (1 - exp((-(Q + Q_L) * t) / V)))
+    } else {
+      return(NA)
+    }
+  })
+  
+  C0 <- (G * (1 - epsilon_L)) / (Q + Q_L) * (1 - exp((-(Q + Q_L) * t_g) / V))
+  
+  C_decay <- sapply(time_vector, function(t) {
+    if (t > t_g) {
+      return(C0 * exp((-(Q + Q_L) * (t - t_g)) / V))
+    } else {
+      return(NA)
+    }
+  })
+  
+  return(list(time = time_vector, concentration_rise = C_rise, concentration_decay = C_decay))
+}
 
 
 # Define the model parameters with units
@@ -18,3 +43,4 @@ V <- 100    # m^3
 V_N <- 8    # Near-field volume in m^3
 beta <- 5   # Near-field ventilation rate in m^3/min
 gamma <- 0.25
+
