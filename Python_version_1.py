34dbc67bb6ffc2fc6f24578698346c59b69335f1
siweixie <146999962+siweixie@users.noreply.github.com>
2023-11-18 03:18:59 -0500

    Update Python_version_1.py

--- Python_version_1.py
+++ Python_version_1.py
@@ -2,26 +2,19 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy.stats import shapiro, wilcoxon
 
-# Define Model 100 with the gamma factor
+# Define the models in Python
 def model_100(G, Q, gamma):
     C_steady = gamma * G / Q
     return C_steady
 
-# Define Model 101
 def model_101(G, Q, V, t_g, T):
     time_vector = np.arange(0, T + 1)
     C_rise = [(G / Q) * (1 - np.exp((-Q * t) / V)) if t <= t_g else np.nan for t in time_vector]
     C0 = (G / Q) * (1 - np.exp(-Q * t_g / V))
-    C_decay = [(C0 * np.exp(-Q * (t - t_g) / V)) if t > t_g else np.nan for t in time_vector]
+    C_decay = [C0 * np.exp(-Q * (t - t_g) / V) if t > t_g else np.nan for t in time_vector]
     C_combined = np.array([x if not np.isnan(x) else y for x, y in zip(C_rise, C_decay)])
     return time_vector, C_combined
 
-# Define Model 102
-def model_102(G, Q, Q_R, epsilon_RF, gamma):
-    C_bar = (gamma * G) / (Q + epsilon_RF * Q_R)
-    return C_bar
-
-# Define Model 103
 def model_103(G, Q, Q_R, epsilon_RF, V, t_g, T):
     Q_with_RF = Q + epsilon_RF * Q_R
     return model_101(G, Q_with_RF, V, t_g, T)
@@ -51,20 +44,25 @@ plt.ylabel('Concentration (mg/m^3)')
 plt.legend()
 plt.show()
 
+# Calculate Mean Squared Error and R^2
+mse = np.mean((concentration_101 - concentration_103)**2)
+r2 = np.corrcoef(concentration_101, concentration_103)[0, 1]**2
+
 # Statistical Analysis
-# Shapiro-Wilk test
-_, p_value_101 = shapiro(concentration_101[~np.isnan(concentration_101)])
-_, p_value_103 = shapiro(concentration_103[~np.isnan(concentration_103)])
+# Shapiro-Wilk test for normality
+normality_test_101 = shapiro(concentration_101[~np.isnan(concentration_101)])
+normality_test_103 = shapiro(concentration_103[~np.isnan(concentration_103)])
 
-print('Shapiro test for Model 101 p-value:', p_value_101)
-print('Shapiro test for Model 103 p-value:', p_value_103)
+# Wilcoxon Signed-Rank Test (if distributions are not normal)
+if normality_test_101.pvalue < 0.05 and normality_test_103.pvalue < 0.05:
+    # Ensuring equal length for the test
+    min_length = min(len(concentration_101[~np.isnan(concentration_101)]), 
+                     len(concentration_103[~np.isnan(concentration_103)]))
+    wilcoxon_test = wilcoxon(concentration_101[:min_length], 
+                             concentration_103[:min_length])
 
-# Wilcoxon signed-rank test (if both distributions are not normal)
-if p_value_101 < 0.05 and p_value_103 < 0.05:
-    min_length = min(len(concentration_101), len(concentration_103))
-    stat, p_value = wilcoxon(concentration_101[:min_length], concentration_103[:min_length])
-    print('Wilcoxon test statistic:', stat)
-    print('Wilcoxon test p-value:', p_value)
+mse, r2, normality_test_101, normality_test_103, wilcoxon_test
+
 
 
 
