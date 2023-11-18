acd19ab1482277cac5c2d62fceda1e0c23c48577
siweixie <146999962+siweixie@users.noreply.github.com>
2023-11-18 00:18:58 -0500

    Update Models_one_box_2.R

--- Models_one_box_2.R
+++ Models_one_box_2.R
@@ -30,3 +30,24 @@ model_105 <- function(G, Q, Q_L, epsilon_L, V, t_g, T) {
   
   return(list(time = time_vector, concentration_rise = C_rise, concentration_decay = C_decay))
 }
+
+
+# Define Model 106
+model_106 <- function(G, Q, Q_L, epsilon_L, Q_R, epsilon_RF, gamma) {
+  C_avg <- (gamma * G * (1 - epsilon_L)) / ((Q + epsilon_RF * Q_R) + Q_L)
+  C_L_avg <- C_avg + (epsilon_L * gamma * G) / Q_L
+  C_RF <- C_avg * (1 - epsilon_RF)
+  return(list(C_avg = C_avg, C_L_avg = C_L_avg, C_RF = C_RF))
+}
+
+
+model_107 <- function(G, Q, Q_L, epsilon_L, Q_R, epsilon_RF, V, t_g, T) {
+  Q_instead <- Q + epsilon_RF * Q_R
+  return(model_105(G, Q_instead, Q_L, epsilon_L, V, t_g, T))
+}
+
+
+results_104 <- model_104(G, Q, Q_L, epsilon_L, gamma)
+results_105 <- model_105(G, Q, Q_L, epsilon_L, V, t_g, T)
+results_106 <- model_106(G, Q, Q_L, epsilon_L, Q_R, epsilon_RF, gamma)
+results_107 <- model_107(G, Q, Q_L, epsilon_L, Q_R, epsilon_RF, V, t_g, T)
