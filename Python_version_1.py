023bb0604e6a87ca7bc68e7a1bad678fb11b4661
siweixie <146999962+siweixie@users.noreply.github.com>
2023-11-18 02:53:49 -0500

    Update Python_version_1.py

--- Python_version_1.py
+++ Python_version_1.py
@@ -1,6 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from scipy.stats import shapiro, wilcoxon 
+from scipy.stats import shapiro, wilcoxon
 
 # Define Model 100 with the gamma factor
 def model_100(G, Q, gamma):
@@ -9,50 +9,37 @@ def model_100(G, Q, gamma):
 
 # Define Model 101
 def model_101(G, Q, V, t_g, T):
-    time_vector = np.arange(0, T+1)
+    time_vector = np.arange(0, T + 1)
     C_rise = [(G / Q) * (1 - np.exp((-Q * t) / V)) if t <= t_g else np.nan for t in time_vector]
     C0 = (G / Q) * (1 - np.exp(-Q * t_g / V))
-    C_decay = [C0 * np.exp(-Q * (t - t_g) / V) if t > t_g else np.nan for t in time_vector]
-    return time_vector, np.array(C_rise), np.array(C_decay)
-
-# Define Model 102
-def model_102(G, Q, Q_R, epsilon_RF, gamma):
-    C_bar = (gamma * G) / (Q + epsilon_RF * Q_R)
-    C_RF = C_bar * (1 - epsilon_RF)
-    return C_bar, C_RF
+    C_decay = [(C0 * np.exp(-Q * (t - t_g) / V)) if t > t_g else np.nan for t in time_vector]
+    C_combined = np.array([x if not np.isnan(x) else y for x, y in zip(C_rise, C_decay)])
+    return time_vector, C_combined
 
 # Define Model 103
 def model_103(G, Q, Q_R, epsilon_RF, V, t_g, T):
-    Q_instead = Q + epsilon_RF * Q_R
-    return model_101(G, Q_instead, V, t_g, T)
+    Q_with_RF = Q + epsilon_RF * Q_R
+    return model_101(G, Q_with_RF, V, t_g, T)
 
 # Set the parameters
 T = 60   # Total time (minutes)
 t_g = 15 # Time of generation (minutes)
 G = 100  # mg/min
 Q = 20   # m^3/min
+Q_R = 5  # m^3/min
 epsilon_RF = 0.9  # Efficiency of recirculation filtration
 V = 100  # m^3
 gamma = 0.25  
-Q_R = 5  # m^3/min
 
-# Perform calculations for models
+# Perform calculations
 C_steady_100 = model_100(G, Q, gamma)
-time_101, C_rise_101, C_decay_101 = model_101(G, Q, V, t_g, T)
-C_bar_102, C_RF_102 = model_102(G, Q, Q_R, epsilon_RF, gamma)
-time_103, C_rise_103, C_decay_103 = model_103(G, Q, Q_R, epsilon_RF, V, t_g, T)
-
-# Remove NaN values for statistical tests and visualization
-valid_indices_101 = ~np.isnan(C_rise_101)
-valid_indices_103 = ~np.isnan(C_rise_103)
+time_101, concentration_101 = model_101(G, Q, V, t_g, T)
+time_103, concentration_103 = model_103(G, Q, Q_R, epsilon_RF, V, t_g, T)
 
 # Visualization: Model 101 vs Model 103
 plt.figure(figsize=(10, 5))
-plt.plot(time_101[valid_indices_101], C_rise_101[valid_indices_101], label='Model 101 Rise')
-plt.plot(time_101[~valid_indices_101], C_decay_101[~valid_indices_101], label='Model 101 Decay')
-plt.plot(time_103[valid_indices_103], C_rise_103[valid_indices_103], label='Model 103 Rise', linestyle='--')
-plt.plot(time_103[~valid_indices_103], C_decay_103[~valid_indices_103], label='Model 103 Decay', linestyle='--')
-plt.axhline(y=C_steady_100, color='red', linestyle='-.', label='Model 100 Steady State')
+plt.plot(time_101, concentration_101, label='Model 101')
+plt.plot(time_103, concentration_103, label='Model 103', linestyle='--')
 plt.title('Model 101 vs Model 103')
 plt.xlabel('Time (minutes)')
 plt.ylabel('Concentration (mg/m^3)')
@@ -60,19 +47,19 @@ plt.legend()
 plt.show()
 
 # Statistical Analysis
-# Remove NaN values for statistical tests
-C_101 = C_rise_101[valid_indices_101]
-C_103 = C_rise_103[valid_indices_103]
+# Shapiro-Wilk test
+_, p_value_101 = shapiro(concentration_101[~np.isnan(concentration_101)])
+_, p_value_103 = shapiro(concentration_103[~np.isnan(concentration_103)])
 
-# Normality test
-print('Shapiro test for Model 101:', shapiro(C_101))
-print('Shapiro test for Model 103:', shapiro(C_103))
+print('Shapiro test for Model 101 p-value:', p_value_101)
+print('Shapiro test for Model 103 p-value:', p_value_103)
 
-# If it is not a normal distribution (p < 0.05), using Wilcoxon test
-p_value_101 = shapiro(C_101).pvalue
-p_value_103 = shapiro(C_103).pvalue
+# Wilcoxon signed-rank test (if both distributions are not normal)
 if p_value_101 < 0.05 and p_value_103 < 0.05:
-    w_stat, p_value_wilcox = wilcoxon(C_101, C_103)
-    print('Wilcoxon test statistic:', w_stat)
-    print('Wilcoxon test p-value:', p_value_wilcox)
+    min_length = min(len(concentration_101), len(concentration_103))
+    stat, p_value = wilcoxon(concentration_101[:min_length], concentration_103[:min_length])
+    print('Wilcoxon test statistic:', stat)
+    print('Wilcoxon test p-value:', p_value)
+
+
 
