a2275399189721b40c44521e5e07baba00f6af9b
siweixie <146999962+siweixie@users.noreply.github.com>
2023-11-18 19:08:45 -0500

    Update Models_two_box_1.R

--- Models_two_box_1.R
+++ Models_two_box_1.R
@@ -51,9 +51,9 @@ model_201 <- function(G, Q, V, V_N, beta, t_g, T) {
            exp(lambda_2 * t_g)
 
   C_N0 <- C_F + G * ((beta * Q + lambda_2 * V_N * (beta + Q)) / (beta * Q * V_N * (lambda_1 - lambda_2))) * 
-          exp(lambda_1 * t) - G * 
+          exp(lambda_1 * t_g) - G * 
           ((beta * Q + lambda_1 * V_N * (beta + Q)) / (beta * Q * V_N * (lambda_1 - lambda_2))) *
-          exp(lambda_2 * t)
+          exp(lambda_2 * t_g)
   
   C_F_decay <- sapply(time_vector, function(t) {
     if (t > t_g) {
