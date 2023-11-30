4e1e67d897ac49cc0cd56065cc7c42f6c509d97d
siweixie <146999962+siweixie@users.noreply.github.com>
2023-11-30 11:57:35 -0500

    Update Models_two_box_3.R

--- Models_two_box_3.R
+++ Models_two_box_3.R
@@ -1,1 +1,11 @@
+# Model 208
 
+model_208 <- function(G, Q, Q_L, beta, gamma, epsilon_L) {
+  epsilon_N <- Q_L / (Q_L + beta)
+  
+  C_F <- (gamma * G * (1 - epsilon_L) * (1 - epsilon_N)) / (Q + Q_L)
+  C_N <- C_F + ((gamma * G * (1 - epsilon_L) * epsilon_N) / Q_L)
+  C_L_E <- C_N + ((gamma * G * epsilon_L) / Q_L)
+  
+  return(list(C_F = C_F, C_N = C_N, C_L_E = C_L_E))
+}
