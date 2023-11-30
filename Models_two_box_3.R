35e68dca9191665b4ac728e19b09fdf389804c1f
siweixie <146999962+siweixie@users.noreply.github.com>
2023-11-30 12:08:24 -0500

    Update Models_two_box_3.R

--- Models_two_box_3.R
+++ Models_two_box_3.R
@@ -14,3 +14,22 @@ model_208 <- function(G, Q, Q_L, beta, gamma, epsilon_L, epsilon_LF) {
 # Run Model 208
 results_208 <- model_208(G, Q, Q_L, beta, gamma, epsilon_L, epsilon_LF)
                     
+
+# Model 209
+                   
+                    
+# Model 210
+
+model_210 <- function(G, Q, Q_L, beta, gamma, epsilon_L, epsilon_LF, epsilon_RF, Q_R) {
+  epsilon_N <- Q_L / (Q_L + beta)
+  
+  C_F <- (gamma * G * ((1 - epsilon_L * epsilon_LF - epsilon_L * epsilon_LF * (1 - epsilon_N)))) / 
+  (Q + epsilon_RF * Q_R + epsilon_LF * Q_L)
+  C_N <- C_F + ((gamma * G * (1 - epsilon_L) * epsilon_N) / Q_L)
+  
+  return(list(C_F = C_F, C_N = C_N))
+}
+
+# Run Model 210
+results_210 <- model_210(G, Q, Q_L, beta, gamma, epsilon_L, epsilon_LF, epsilon_RF, Q_R)
+
